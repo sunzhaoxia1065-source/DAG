20fb3a197a8a1670ad99be3bb35212a3853bde8f
sunzhaoxia1065-source <sunzhaoxia1065@gmail.com>
2026-09-23 13:34:47 +0800

    Delete report directory

--- report/report_dash/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import
-
-__all__ = ["report"]
-
-from ts_benchmark.report.report_dash.app import report
--- report/report_dash/memory.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import
-
-
-# storing global read-only data, which should only be set during main app initialization
-READONLY_MEMORY = {}
